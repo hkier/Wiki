873ecfc12f7d62f5062bfe7b004352205353c6c8
Howard Kier <howard@Howards-MacBook-Air.local>
2017-08-04 17:02:04 -0500

    End of Synching our models

---
+++ models/index.js
@@ -0,0 +1,35 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
+
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING,
+        validate:{isEmail: true}
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
